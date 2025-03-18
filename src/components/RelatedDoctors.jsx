69b09efa1e1bb05fc3124b6e56e46ceaf48d698d
unknown <vinaykushwah890@gmail.com>
2025-03-18 18:25:02 +0530

    doctors page complete

--- src/components/RelatedDoctors.jsx
+++ src/components/RelatedDoctors.jsx
@@ -1,10 +1,20 @@
-import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
-const TopDoctors = () => {
-  const navigate = useNavigate();
+const RelatedDoctors = ({ speciality, docId }) => {
   const { doctors } = useContext(AppContext);
+  const [relDocs, setRelDocs] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (doctors.length > 0 && speciality) {
+      const doctorsData = doctors.filter(
+        (doc) => doc.speciality === speciality && doc._id !== docId
+      );
+      setRelDocs(doctorsData);
+    }
+  }, [doctors, speciality, docId]);
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
@@ -12,13 +22,10 @@ const TopDoctors = () => {
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 5).map((doctor, index) => (
+        {relDocs.slice(0, 10).map((doctor, index) => (
           <div
             key={index}
-            onClick={() => {
-              navigate(`/appointment/${doctor._id}`);
-              scrollTo(0, 0);
-            }}
+            onClick={() => navigate(`/appointment/${doctor._id}`)}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
           >
             <img src={doctor.image} alt="" className="bg-blue-50" />
@@ -40,4 +47,4 @@ const TopDoctors = () => {
   );
 };
 
-export default TopDoctors;
+export default RelatedDoctors;
