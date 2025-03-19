b464ecefa2930d1c9d0d3d73ba59b670029e15b9
unknown <vinaykushwah890@gmail.com>
2025-03-19 17:16:34 +0530

    contact us page complete

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,13 +3,34 @@ import { assets } from "../assets/assets_frontend/assets";
 
 const Contact = () => {
   return (
-    <div className="flex flex-row">
-      <div>
-        <img src={assets.contact_image} alt="" />
+    <div className="">
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          CONTACT <span className="text-gray-700 font-semibold">US</span>
+        </p>
       </div>
-      <div>
-        <div>
-          <p>Our Office</p>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+        <img
+          src={assets.contact_image}
+          alt=""
+          className="w-full md:max-w-[360px]"
+        />
+
+        <div className="flex flex-col justify-start items-start gap-6">
+          <p className="font-semibold text-lg text-gray-600">OUR OFFICE</p>
+          <p className="text-gray-500">
+            54709 Willms Station <br /> Suite 350, Washington, USA
+          </p>
+          <p className="text-gray-500">
+            Tel:(415) 434 - 232 <br /> Email : text12@gamil.com
+          </p>
+          <p className="font-semibold text-lg">Careers at PRESCRIPTO</p>
+          <p className="text-gray-500">
+            Learn more about our teams and job opening
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 ">
+            Explore Jobs
+          </button>
         </div>
       </div>
     </div>
