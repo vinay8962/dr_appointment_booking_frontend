25ec53c711fcfabc7296f9079b5f1258b89dcb2d
unknown <vinaykushwah890@gmail.com>
2025-03-21 17:15:17 +0530

    complete my appointment page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  //   const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
   const [token, setToken] = useState(true);
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-gray-400">
@@ -71,6 +71,58 @@ const Navbar = () => {
             Create Account
           </button>
         )}
+        <img
+          onClick={() => setShowMenu(true)}
+          src={assets.menu_icon}
+          className="w-6 md:hidden "
+          alt=""
+        />
+        {/* mobile menu */}
+        <div
+          className={`${
+            showMenu ? "fixed w-full" : "h-0 w-0"
+          } md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}
+        >
+          <div className="flex items-center justify-between px-5 py-6">
+            <img src={assets.logo} alt="" className="w-36" />
+            <img
+              src={assets.cross_icon}
+              className="w-7"
+              onClick={() => setShowMenu(false)}
+              alt=""
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink
+              to="/"
+              onClick={() => setShowMenu(false)}
+              className="px-4 py-2 rounded inline-block"
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to="/doctors"
+              onClick={() => setShowMenu(false)}
+              className="px-4 py-2 rounded inline-block"
+            >
+              <li>All Doctors</li>
+            </NavLink>
+            <NavLink
+              to="/about"
+              onClick={() => setShowMenu(false)}
+              className="px-4 py-2 rounded inline-block"
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to="/contact"
+              onClick={() => setShowMenu(false)}
+              className="px-4 py-2 rounded inline-block"
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/pages/Doctor.jsx
+++ src/pages/Doctor.jsx
@@ -6,6 +6,7 @@ const Doctor = () => {
   const { speciality } = useParams();
   const { doctors } = useContext(AppContext);
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter, setShowFilter] = useState(false);
   const navigate = useNavigate();
   const applyFilter = () => {
     if (speciality) {
@@ -24,7 +25,19 @@ const Doctor = () => {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <button
+          className={`1py-1 px-3 border rounded text-sm transition-all sm:hidden ${
+            showFilter ? "bg-primary text-white" : ""
+          }`}
+          onClick={() => setShowFilter((prev) => !prev)}
+        >
+          Filters
+        </button>
+        <div
+          className={`flex flex-col gap-4 text-sm text-gray-600 ${
+            showFilter ? "flex" : "hidden sm:flex"
+          }`}
+        >
           <p
             onClick={() =>
               speciality === "General physician"
